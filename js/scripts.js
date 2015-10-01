053e78648524a1f47dcd35c30918dedc85a17194
lizlove <elizabethlovero@gmail.com>
2015-10-01 14:09:40 -0400

    add carousel and update copy" "

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
+ 
+  $("#owl-container").owlCarousel({
+    items : 3,
+    navigation: true,
+    navigationText: [
+      "<i class='icon-chevron-left icon-black'><</i>",
+      "<i class='icon-chevron-right icon-black'>></i>"
+      ],
+  });
 
 });
